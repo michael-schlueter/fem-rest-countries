10ea5d70437f3a961789b1d3179572ff1eb3ba4a
rulo <schluet0r4life@gmail.com>
2024-10-10 20:33:21 +0200

    add custom font, fontsizes and colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,29 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      "dark-blue": {
+        "100": "hsl(209, 23%, 22%)",
+        "200": "hsl(207, 26%, 17%)",
+        "300": "hsl(200, 15%, 8%)",
+      },
+      "dark-gray": "hsl(0, 0%, 52%)",
+      "light-gray": "hsl(0, 0%, 98%)",
+      white: "hsl(0, 0%, 100%",
+    },
+    fontSizes: {
+      sm: ["0.75rem"],
+      base: ["0.875rem", "1rem"],
+      "base-lh": ["0.875rem", "1.25rem"],
+      lg: ["1rem"],
+      xl: ["1.125rem", "1.625rem"],
+      "2xl": ["1.5rem"],
+    },
+    extend: {
+      fontFamily: {
+        "nunito-sans": ["Nunito Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
