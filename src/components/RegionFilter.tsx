2b5bf93ea71aa54eb426a2dc5f250cd300bbee83
rulo <schluet0r4life@gmail.com>
2024-10-14 18:35:49 +0200

    add logic for filtering countries

--- src/components/RegionFilter.tsx
+++ src/components/RegionFilter.tsx
@@ -5,16 +5,37 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { useState } from "react";
+import { Countries } from "@/routes/index";
 
-export default function RegionFilter() {
+type RegionFilterProps = {
+  setFilteredCountries: React.Dispatch<React.SetStateAction<Countries>>;
+  defaultCountries: Countries;
+};
+
+export default function RegionFilter({
+  setFilteredCountries,
+  defaultCountries,
+}: RegionFilterProps) {
+  const [region, setRegion] = useState("");
+
+  const handleChange = (value: string) => {
+    setRegion(value);
+    setFilteredCountries(
+      defaultCountries.filter((country) => {
+        return country.region.toLowerCase() === value;
+      })
+    );
+  };
+
   return (
-    <Select>
+    <Select value={region} onValueChange={handleChange}>
       <SelectTrigger className="w-[200px] text-xs text-dark-blue-300 border-none shadow-md ">
         <SelectValue placeholder="Filter by Region" />
       </SelectTrigger>
       <SelectContent className="text-xs text-dark-blue-300 border-none">
         <SelectItem value="africa">Africa</SelectItem>
-        <SelectItem value="america">America</SelectItem>
+        <SelectItem value="americas">America</SelectItem>
         <SelectItem value="asia">Asia</SelectItem>
         <SelectItem value="europe">Europe</SelectItem>
         <SelectItem value="oceania">Oceania</SelectItem>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,20 +5,35 @@ import SearchBar from "../components/SearchBar";
 import CountryCard from "@/components/CountryCard";
 import countries from "@/data.json";
 
+export type Countries = typeof countries;
+
 export const Route = createFileRoute("/")({
   component: HomeComponent,
 });
 
 function HomeComponent() {
+  const [defaultCountries, setDefaultCountries] = React.useState(countries);
+  const [filteredCountries, setFilteredCountries] = React.useState(countries);
+  
   return (
     <main className="px-4 pt-6 flex flex-col gap-[30px]">
       <div className="flex flex-col gap-10">
         <SearchBar />
-        <RegionFilter />
+        <RegionFilter
+          defaultCountries={defaultCountries}
+          setFilteredCountries={setFilteredCountries}
+        />
       </div>
       <div className="max-w-[336px] grid gap-10 mx-auto ov">
-        {countries.map((country) => (
-          <CountryCard name={country.name} imagePath={country.flags["svg"]} region={country.region} capital={country.capital} population={country.population} />
+        {filteredCountries.map((country) => (
+          <CountryCard
+            key={country.alpha3Code}
+            name={country.name}
+            imagePath={country.flags["svg"]}
+            region={country.region}
+            capital={country.capital}
+            population={country.population}
+          />
         ))}
       </div>
     </main>
