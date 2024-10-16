14a5dd660f5f228eec2a41785f0347795cf2205d
rulo <schluet0r4life@gmail.com>
2024-10-16 18:07:14 +0200

    adjust flag image sizes and truncate headings

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -6,21 +6,34 @@ type CountryCardProps = {
   population: number;
 };
 
-export default function CountryCard({ name, imagePath, region, capital = "No capital", population }: CountryCardProps) {
+export default function CountryCard({
+  name,
+  imagePath,
+  region,
+  capital = "No capital",
+  population,
+}: CountryCardProps) {
   return (
     <div className="rounded-[5px] shadow-md overflow-hidden">
-      <img className="w-full" src={imagePath} />
+      <div className="w-[264px] h-[160px] overflow-hidden">
+        <img className="w-full h-full object-cover" src={imagePath} />
+      </div>
       <div className="grid gap-4 p-6 pb-[46px] bg-white dark:bg-dark-blue-400">
-        <h2 className="text-dark-blue-300 dark:text-white text-2xl font-extrabold">{name}</h2>
-        <div>
+        <h2 className="text-dark-blue-300 dark:text-white text-2xl font-extrabold truncate">
+          {name}
+        </h2>
+        <div className="grid gap-2">
           <p className="text-base text-dark-blue-300 dark:text-white font-light">
-            <span className="font-semibold">Population: </span>{population}
+            <span className="font-semibold">Population: </span>
+            {population}
           </p>
           <p className="text-base text-dark-blue-300 dark:text-white font-light">
-            <span className="font-semibold">Region: </span>{region}
+            <span className="font-semibold">Region: </span>
+            {region}
           </p>
           <p className="text-base text-dark-blue-300 dark:text-white font-light">
-            <span className="font-semibold">Capital: </span>{capital}
+            <span className="font-semibold">Capital: </span>
+            {capital}
           </p>
         </div>
       </div>
