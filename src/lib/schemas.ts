87b90fe74f8ba91b4ccd5b0755b895cbf55f5b47
rulo <schluet0r4life@gmail.com>
2024-10-25 13:39:05 +0200

    add dynamic rendering of CountryDetails

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -14,3 +14,35 @@ export const countriesSchema = z.array(
     }),
   })
 );
+
+const currencySchema = z.object({
+  name: z.string(),
+  symbol: z.string().optional(),
+});
+
+const languageSchema = z.string();
+;
+
+const nameSchema = z.object({
+  common: z.string(),
+  nativeName: z.record(
+    z.object({
+      official: z.string(),
+    })
+  )
+})
+
+export const countrySchema = z.object({
+  name: nameSchema,
+  flags: z.object({
+    svg: z.string().url(),
+  }),
+  population: z.number(),
+  region: z.string(),
+  subregion: z.string(),
+  capital: z.array(z.string()),
+  tld: z.array(z.string()),
+  currencies: z.record(currencySchema),
+  languages: z.record(languageSchema),
+  borders: z.array(z.string()),
+});
--- src/routes/countries.$countryCode.tsx
+++ src/routes/countries.$countryCode.tsx
@@ -1,5 +1,11 @@
+import { countrySchema } from "@/lib/schemas";
 import { useQuery } from "@tanstack/react-query";
-import { createFileRoute, Link, useParams, useRouter } from "@tanstack/react-router";
+import {
+  createFileRoute,
+  Link,
+  useParams,
+  useRouter,
+} from "@tanstack/react-router";
 import { ArrowLeft } from "lucide-react";
 
 export const Route = createFileRoute("/countries/$countryCode")({
@@ -9,6 +15,29 @@ export const Route = createFileRoute("/countries/$countryCode")({
 function CountryDetails() {
   const { countryCode } = useParams({ strict: false });
   const router = useRouter();
+
+  const {
+    isPending,
+    error,
+    data: country,
+  } = useQuery({
+    queryKey: ["country", countryCode],
+    queryFn: getCountry,
+  });
+
+  if (isPending) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  async function getCountry() {
+    const response = await fetch(
+      `https://restcountries.com/v3.1/alpha/${countryCode}`
+    );
+    if (!response.ok) {
+      throw new Error("Network Error");
+    }
+    const data = await response.json();
+    return countrySchema.parse(data[0]);
+  }
 
   return (
     <main className="grid gap-16 px-7 pt-10 pb-[60px] xl:p-[80px]">
@@ -23,43 +52,83 @@ function CountryDetails() {
         <div className="max-w-[320px] xl:max-w-none xl:w-[560px] xl:h-[400px] overflow-hidden rounded-[5px] xl:rounded-[10px]">
           <img
             className="w-full h-full object-cover"
-            src="https://flagcdn.com/be.svg"
+            src={country.flags["svg"]}
           />
         </div>
         <div className="grid gap-[34px]">
           <div className="grid gap-4 xl:gap-6">
             <h2 className="text-dark-blue-300 dark:text-white font-extrabold text-[22px] xl:text-[32px]">
-              Belgium
+              {country.name.common}
             </h2>
             <div className="flex flex-col xl:flex-row gap-8 xl:gap-[118px] text-dark-blue-300 dark:text-white text-[14] leading-8">
               <div className="grid">
                 <p className="font-light">
-                  <span className="font-semibold">Native Name: </span>Belgie
+                  <span className="font-semibold">Native Name: </span>
+                  {Object.values(country.name.nativeName).map(
+                    (native, index) => (
+                      <span key={index}>
+                        {native.official}
+                        {index <
+                        Object.values(country.name.nativeName).length - 1
+                          ? ", "
+                          : ""}
+                      </span>
+                    )
+                  )}
                 </p>
                 <p className="font-light">
-                  <span className="font-semibold">Population: </span>11,319,511
+                  <span className="font-semibold">Population: </span>
+                  {country.population.toLocaleString()}
                 </p>
                 <p className="font-light">
-                  <span className="font-semibold">Region: </span>Europe
+                  <span className="font-semibold">Region: </span>
+                  {country.region}
                 </p>
                 <p className="font-light">
-                  <span className="font-semibold">Sub Region: </span>Western
-                  Europe
+                  <span className="font-semibold">Sub Region: </span>
+                  {country.subregion}
                 </p>
                 <p className="font-light">
-                  <span className="font-semibold">Capital: </span>Brussels
+                  <span className="font-semibold">Capital: </span>
+                  {country.capital.map((city, index) => (
+                    <span key={index}>
+                      {city}
+                      {index < country.capital.length - 1 ? ", " : ""}
+                    </span>
+                  ))}
                 </p>
               </div>
               <div className="flex flex-col">
                 <p className="font-light">
-                  <span className="font-semibold">Top Level Domain: </span>.be
+                  <span className="font-semibold">Top Level Domain: </span>
+                  {country.tld.map((domain, index) => (
+                    <span key={index}>
+                      {domain}
+                      {index < country.tld.length - 1 ? ", " : ""}
+                    </span>
+                  ))}
                 </p>
                 <p className="font-light">
-                  <span className="font-semibold">Currencies: </span>Euro
+                  <span className="font-semibold">Currencies: </span>
+                  {Object.values(country.currencies).map((curency, index) => (
+                    <span key={index}>
+                      {curency.name}
+                      {index < Object.values(country.currencies).length - 1
+                        ? ", "
+                        : ""}
+                    </span>
+                  ))}
                 </p>
                 <p className="font-light">
-                  <span className="font-semibold">Languages: </span>Dutch,
-                  French, German
+                  <span className="font-semibold">Languages: </span>
+                  {Object.values(country.languages).map((language, index) => (
+                    <span key={index}>
+                      {language}
+                      {index < Object.values(country.languages).length - 1
+                        ? ", "
+                        : ""}
+                    </span>
+                  ))}
                 </p>
               </div>
             </div>
@@ -69,26 +138,18 @@ function CountryDetails() {
               Border Countries:
             </h3>
             <div className="flex gap-[10px] flex-wrap text-[12px] text-dark-blue-300 dark:text-white">
-              <Link to="/countries/$countryCode" params={{
-                countryCode: "Germany"
-              }} className="bg-white dark:bg-dark-blue-100 flex justify-center items-center py-1.5 min-w-24 rounded-[2px] shadow-custom-5">
-                Germany
-              </Link>
-              <Link to="/countries/$countryCode" params={{
-                countryCode: "Netherlands"
-              }} className="bg-white dark:bg-dark-blue-100 flex justify-center items-center py-1.5 min-w-24 rounded-[2px] shadow-custom-5">
-                Netherlands
-              </Link>
-              <Link to="/countries/$countryCode" params={{
-                countryCode: "France"
-              }} className="bg-white dark:bg-dark-blue-100 flex justify-center items-center py-1.5 min-w-24 rounded-[2px] shadow-custom-5">
-                France
-              </Link>
-              <Link to="/countries/$countryCode" params={{
-                countryCode: "France"
-              }} className="bg-white dark:bg-dark-blue-100 flex justify-center items-center py-1.5 min-w-24 rounded-[2px] shadow-custom-5">
-                France
-              </Link>
+              {country.borders.map((borderCountry, index) => (
+                <Link
+                  key={index}
+                  to={`/countries/${borderCountry}`}
+                  params={{
+                    countryCode: borderCountry
+                  }}
+                  className="bg-white dark:bg-dark-blue-100 flex justify-center items-center py-1.5 min-w-24 rounded-[2px] shadow-custom-5"
+                >
+                  {borderCountry}
+                </Link>
+              ))}
             </div>
           </div>
         </div>
