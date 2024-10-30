fc626a1d6cae56a3ed51ec0cd7aa8d67fdb5de2f
rulo <schluet0r4life@gmail.com>
2024-10-30 20:18:40 +0100

    extract Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import { Moon } from "lucide-react";
+
+type HeaderProps = {
+  onHandleToggleDarkMode: () => void;
+};
+
+export default function Header({ onHandleToggleDarkMode }: HeaderProps) {
+  return (
+    <header className="bg-white dark:bg-dark-blue-400 flex justify-between px-4 lg:px-20 py-[30px] shadow-custom-2">
+      <h1 className="text-lg md:text-2xl text-dark-blue-300 dark:text-white font-extrabold">
+        Where in the world?
+      </h1>
+      <button
+        onClick={onHandleToggleDarkMode}
+        className="flex gap-2 items-center text-dark-blue-300 dark:text-white"
+        aria-label="Toggle Dark Mode"
+      >
+        <Moon className="h-4 w-4 md:h-5 md:w-5 dark:fill-white" />
+
+        <span className="text-sm md:text-xl font-semibold">Dark Mode</span>
+      </button>
+    </header>
+  );
+}
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,5 @@
+import Header from "@/components/Header";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-import { Moon } from "lucide-react";
 import { useEffect, useState } from "react";
 
 export const Route = createRootRoute({
@@ -29,19 +28,7 @@ function RootComponent() {
 
   return (
     <div className="min-h-screen bg-light-gray dark:bg-dark-blue-200">
-      <header className="bg-white dark:bg-dark-blue-400 flex justify-between px-4 lg:px-20 py-[30px] shadow-custom-2">
-        <h1 className="text-lg md:text-2xl text-dark-blue-300 dark:text-white font-extrabold">
-          Where in the world?
-        </h1>
-        <button
-          onClick={handleToggleDarkMode}
-          className="flex gap-2 items-center text-dark-blue-300 dark:text-white"
-        >
-          <Moon className="h-4 w-4 md:h-5 md:w-5 dark:fill-white" />
-
-          <span className="text-sm md:text-xl font-semibold">Dark Mode</span>
-        </button>
-      </header>
+      <Header onHandleToggleDarkMode={handleToggleDarkMode} />
       <Outlet />
     </div>
   );
