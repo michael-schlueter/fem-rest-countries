e313107888d00033df6a5a4698ea8ae09fc5f546
rulo <schluet0r4life@gmail.com>
2024-11-05 14:05:27 +0100

    add loading spinner & memoize filtering logic

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,8 +2,9 @@ import { createFileRoute } from "@tanstack/react-router";
 import RegionFilter from "../components/RegionFilter";
 import SearchBar from "../components/SearchBar";
 import CountryCard from "@/components/CountryCard";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useCountries } from "@/lib/hooks";
+import { Loader2 } from "lucide-react";
 
 export const Route = createFileRoute("/")({
   component: HomeComponent,
@@ -13,32 +14,33 @@ function HomeComponent() {
   const [searchTerm, setSearchTerm] = useState("");
   const [region, setRegion] = useState("");
 
-  const { isPending, error, data: countries } = useCountries();
+  const { isPending, error, data: countries = [] } = useCountries();
 
-  if (isPending) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
 
-  const filteredCountries = countries
-    .filter((country) => {
-      if (searchTerm === "") {
-        return country;
-      } else if (
-        country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
-      ) {
-        return country;
-      } else {
-        return false;
-      }
-    })
-    .filter((filteredCountry) => {
-      if (region === "" || region === "reset") {
-        return filteredCountry;
-      } else if (filteredCountry.region.toLowerCase() === region) {
-        return filteredCountry;
-      } else {
-        return false;
-      }
-    });
+  const filteredCountries = useMemo(() => {
+    return countries
+      .filter((country) => {
+        if (searchTerm === "") {
+          return country;
+        } else if (
+          country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+        ) {
+          return country;
+        } else {
+          return false;
+        }
+      })
+      .filter((filteredCountry) => {
+        if (region === "" || region === "reset") {
+          return filteredCountry;
+        } else if (filteredCountry.region.toLowerCase() === region) {
+          return filteredCountry;
+        } else {
+          return false;
+        }
+      });
+  }, [countries, searchTerm, region]);
 
   return (
     <main className="px-4 lg:px-20 pt-6 md:pt-12 flex flex-col gap-[30px] md:gap-12">
@@ -46,19 +48,25 @@ function HomeComponent() {
         <SearchBar setSearchTerm={setSearchTerm} />
         <RegionFilter region={region} setRegion={setRegion} />
       </div>
-      <div className="w-full grid grid-cols-[repeat(auto-fit,minmax(0,264px))] gap-10 sm:gap-[75px] justify-center">
-        {filteredCountries.map((country) => (
-          <CountryCard
-            key={country.cca3}
-            name={country.name.common}
-            imagePath={country.flags["svg"]}
-            region={country.region}
-            capital={country.capital}
-            population={country.population}
-            countryCode={country.cca3}
-          />
-        ))}
-      </div>
+      {isPending ? (
+        <div className="flex justify-center items-center min-h-[400px]">
+          <Loader2 className="h-8 w-8 animate-spin" />
+        </div>
+      ) : (
+        <div className="w-full grid grid-cols-[repeat(auto-fit,minmax(0,264px))] gap-10 sm:gap-[75px] justify-center">
+          {filteredCountries.map((country) => (
+            <CountryCard
+              key={country.cca3}
+              name={country.name.common}
+              imagePath={country.flags["svg"]}
+              region={country.region}
+              capital={country.capital}
+              population={country.population}
+              countryCode={country.cca3}
+            />
+          ))}
+        </div>
+      )}
     </main>
   );
 }
